734243dcc24306c8daec3be9f1193bf468dac3ed
OGOishi <oishi@nse-ed.com>
2026-03-16 16:27:32 +0900

    greets test06

--- commands/greets.js
+++ commands/greets.js
@@ -9,8 +9,8 @@ module.exports = {
             .setName('language')
             .setDescription('言語を指定します')
             .setRequired(true)
-            /*.addChoices({name:'Japanese',value:'ja'},
-            {name:'English',value:'en'})*/
+            .addChoices({name:'Japanese',value:'ja'},
+            {name:'English',value:'en'})
         ),
     async execute(interaction) {
         /*if (interaction.options.getString('language') === 'Japanese') {*/

9f9f12070675a2987d00c2ca904b693dd2b1f0fe
OGOishi <oishi@nse-ed.com>
2026-03-16 18:13:58 +0900

    add ohayo

---
+++ events/ohayo.js
@@ -0,0 +1,11 @@
+const { Events } = require('discord.js');
+
+//MessageCreateEvent処理(サーバーにメッセージが送信された時の処理)
+client.on(Events.MessageCreate, async message => { //messageに作られたmessageとかいろいろ入る
+    if (message.author.bot) {//メッセージの送信者がBOTなら
+        return;//returnしてこの先の処理をさせない。
+    }
+    if (message.content.startsWith("おはよ")) { //message.content(メッセージの内容)が「おはよ」で始まっていたら
+        await message.channel.send("おはよ"); //botがmessage.channel(メッセージが送信されたチャンネル)に「おはよ」と送信する
+    }
+});
